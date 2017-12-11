36bc9973724117acfe089974d78ce158a0681e75
Mathias Bynens <mathias@qiwi.be>
2017-12-11 11:02:55 +0100

    Use @babel/preset-env
    The yearly presets like preset-es2015 are deprecated, and the best
    practice is to use @babel/preset-env nowadays.
    
    Ref. #27.

--- src/babel-benchmark.js
+++ src/babel-benchmark.js
@@ -2,14 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const Babel = require("@babel/standalone");
+const Babel = require("@babel/core");
 const babylon = require("babylon");
 const fs = require("fs");
 
 const payloads = [
   {
     name: "vue.runtime.esm-nobuble-2.4.4.js",
-    options: { presets: ["es2015"], sourceType: "module" }
+    options: {
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              browsers: ["last 2 versions"],
+              node: 4 // https://github.com/nodejs/Release#release-schedule
+            }
+          }
+        ]
+      ],
+      sourceType: "module"
+    }
   }
 ].map(({ name, options }) => {
   const code = fs.readFileSync(`third_party/${name}`, "utf8");
